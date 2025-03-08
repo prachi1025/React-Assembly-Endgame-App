c0e295f23cb3167c5d859d55feb5bafb6b15a586
prachi1025 <prachipargal123@gmail.com>
2025-03-08 18:20:43 +0530

    add static current word array

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
 import { languages } from "./assets/languages"
-
-const languageChips = languages.map(language => <span className={"language-chip"} style={{ backgroundColor:language.backgroundColor, color:language.color}}>{language.name}</span>)
+import React from "react"
 
 export default function App() {
+
+  const [currentWord, setCurrentWord] = React.useState("react")
+
+  //current word in jsx on the page
+  const gameWord = Array.from(currentWord).map((word, index) => 
+    <span key={index} className="game-word">{word.toLocaleUpperCase()}</span> 
+    )
+  
+  //language chips on the page
+  const languageChips = languages.map(language => <span className={"language-chip"} style={{ backgroundColor:language.backgroundColor, color:language.color}} key={language.name}>{language.name}</span>)
+
   return (
     <main>
       <div className="top-div">
@@ -20,6 +30,11 @@ export default function App() {
       <div className="language-container">
         {languageChips}
       </div>
+
+      <div className="game-word-container">
+        {gameWord}
+      </div>
+      
     </main>
   )
 }
